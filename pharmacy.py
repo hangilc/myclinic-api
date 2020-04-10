d06cb41ef803b6bbe2fbd6387c7d618780d0fe9c
hangilc <hangil@chang.jp>
2020-04-10 22:33:45 +0900

    added pharmacy.py

---
+++ pharmacy.py
@@ -0,0 +1,64 @@
+import os
+from typing import List
+import re
+
+
+class Pharmacy:
+    def __init__(self, name, fax, addr):
+        self.name = name
+        self.fax = fax
+        self.addr = addr
+
+    def __repr__(self):
+        return f"<Pharmacy name={self.name}, fax={self.fax}, addr={self.addr}>"
+
+
+def pharmacy_list_file():
+    return os.getenv("MYCLINIC_PHARMACY_LIST")
+
+
+re_title = re.compile(r'【(.+)】')
+re_fax = re.compile(r'fax:\s*(\+\d+)')
+re_addr = re.compile(r'〒(\d{3}-\d{4})\s+(.+)')
+
+
+def read_pharmacy_list(path: str) -> List[Pharmacy]:
+    result = []
+    name = None
+    fax = None
+    addr = None
+
+    def flush():
+        nonlocal name, fax, addr
+        if name and fax and addr:
+            result.append(Pharmacy(name, fax, addr))
+        name = None
+        fax = None
+        addr = None
+
+    with open(path, "r", encoding="UTF-8") as fp:
+        for line in fp:
+            line = line.strip()
+            if not line:
+                continue
+            m = re_title.match(line)
+            if m:
+                flush()
+                name = m.group(1)
+                continue
+            m = re_fax.match(line)
+            if m:
+                fax = m.group(1)
+                continue
+            m = re_addr.match(line)
+            if m:
+                addr = (m.group(1), m.group(2))
+                continue
+    flush()
+    return result
+
+
+if __name__ == "__main__":
+    pharmacies = read_pharmacy_list(pharmacy_list_file())
+    for p in pharmacies:
+        print(p)
